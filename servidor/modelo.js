536e5f9ead88b05f6839e56f3aa02938f0e7d4e3
FÉLIX CUEVAS MEDINA <Felix.Cuevas@alu.uclm.es>
2022-11-07 20:13:54 +0100

    creación de las clases Barco, Casillas, Tablero y Agua

--- servidor/modelo.js
+++ servidor/modelo.js
@@ -96,6 +96,9 @@ function Juego(){
 function Usuario(nick,juego){
 	this.nick=nick;
 	this.juego=juego;
+	this.tableroPropio;
+	this.tableroRival;
+	this.flota = {}; //podría ser []
 
 	this.crearPartida=function(){
 		return this.juego.crearPartida(this)
@@ -103,6 +106,72 @@ function Usuario(nick,juego){
 
 	this.unirseAPartida=function(codigo){
 		this.juego.unirseAPartida(codigo,this);
+	}
+
+	this.inicializarTableros = function(dim){
+		this.tableroPropio = new Tablero(dim);
+		this.tableroRival = new Tablero(dim);
+	}
+
+	this.inicializarFlota = function(){
+		/*this.inicializarFlota.push(new Barco("b2", 2));
+		this.flota.push(new Barco("b4", 4));*/
+
+		this.flota["b2"] = new Barco("b2", 2);
+		this.flota["b4"] = new Barco("b4", 4);
+		//otros barcos: 1, 3, 5
+	}
+
+	this.colocarBarco = function(nombre, x, y){
+		//Comprobar fase
+		if(partidas.fase=="desplegando"){
+			let barco = this.flota[nombre];
+			this.tableroPropio.colocarBarco(barco, x, y);
+		}
+		//Coloca el barco de nombre en la casilla x,y del tablero proporcionado
+	}
+
+	this.todosDesplegados = function(){
+		for(var key in this.flota){
+			if(!this.flota[key].desplegado){
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	this.barcosDesplegados = function(){
+		this.partida.barcosDesplegados();
+	}
+
+	this.disparar = function(x, y){
+		this.partida.disparar(this.nick, x, y);
+	}
+
+	this.meDisparan = function(x, y){
+		this.tableroPropio.meDisparan(x, y);
+	}
+
+	this.obtenerEstado = function(x, y){
+		return this.tableroPropio.obtenerEstado(x, y);
+	}
+
+	this.marcarEstado = function(estado, x, y){
+		this.tableroRival.marcarEstado(estado, x, y);
+		if(estado=="Agua"){
+			this.partida.cambiarTurno(this.nick);
+		}
+	}
+
+	this.flotaHundida = function(){
+		for(var key in this.flota){
+			if(!this.flota[key].estado == "Hundido"){
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
 
@@ -118,6 +187,8 @@ function Partida(codigo,usr){
 		if (this.hayHueco()){
 			this.jugadores.push(usr);
 			console.log("El usuario " + usr.nick + " se une a la partida " +this.codigo);
+			usr.inicializarTableros(5);
+			usr.inicializarFlota();
 			this.comprobarFase();
 		}
 		else{
@@ -130,7 +201,7 @@ function Partida(codigo,usr){
 
 	this.comprobarFase = function(){
 		if(!this.hayHueco()){
-			this.fase = "jugando";
+			this.fase = "desplegando";
 		}
 	}
 
@@ -151,7 +222,180 @@ function Partida(codigo,usr){
 		return this.fase=="jugando";
 	}
 
+	this.flotasDesplegadas = function(){
+		for(i=0; this.jugadores.length; i++){
+			if(!this.jugadores[i].todosDesplegados()){
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	this.barcosDesplegados = function(){
+		if(this.flotasDesplegadas()){
+			this.fase = "jugando";
+			this.asignarTurnoInicial();
+		}
+	}
+
+	this.asignarTurnoInicial = function(){
+		this.turno = this.jugadores[0];
+	}
+
+	this.cambiarTurno = function(nick){
+		this.turno = this.obtenerRival(nick);
+	}
+
+	this.obtenerRival = function(nick){
+		let rival;
+		for(i=0; i<this.jugadores.length; i++){
+			if(this.jugadores[i].nick != nick){
+				rival = this.jugadores[i];
+			}
+		}
+
+		return rival;
+	}
+
+	this.obtenerJugador = function(nick){
+		let jugador;
+		for(i=0; i<this.jugadores.length; i++){
+			if(this.jugadores[i].nick == nick){
+				jugador = this.jugadores[i];
+			}
+		}
+
+		return jugador;
+	}
+
+	this.disparar = function(nick, x, y){
+		let atacante = this.obtenerJugador();
+		if(this.turno.nick == atacante.nick){
+			let atacado = this.obtenerRival(nick);
+
+			atacado.meDisparan(x, y);
+			let estado = atacado.obtenerEstado(x, y);
+			atacante.marcarEstado(estado, x, y);
+			this.comprobarFin(atacado);
+		} 
+		else {
+			console.log("No es tu turno");
+		}
+	}
+
+	this.comprobarFin = function(jugador){
+		if(jugador.flotaHundida()){
+			this.fase = "Final";
+			console.log("Fin de la partida");
+			console.log("Ganador: " +turno.nick);
+		}
+	}
+
 	this.agregarJugador(this.owner);
+}
+
+function Tablero(size){
+	this.size = size; //filas = columnas = size
+	this.casillas;
+
+	this.crearTablero=function(tam){
+		this.casillas=new Array(tam);
+		for(x=0;x<tam;x++){
+			this.casillas[x]=new Array(tam);
+			for(y=0;y<tam;y++){
+				this.casillas[x][y]=new Casilla(x,y);
+			}
+		}
+	}
+
+	this.colocarBarco = function(barco, x, y){
+		if(this.casillasLibres(x,y, barco.tam)){
+			for(i=x; i<barco.tam; i++){
+				this.casillas[i][y].contiene = barco;
+			}
+			barco.desplegado = true;
+		}
+	}
+
+	this.casillasLibres = function(x, y, tam){
+		for(i=x; i<barco.tam; i++){
+			let casilla = this.casillas[i][y].contiene;
+			if(!contiene.esAgua()){
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	this.meDisparan = function(x, y){
+		this.casillas[x][y].contiene.meDisparan();
+	}
+
+	this.obtenerEstado = function(x, y){
+		return this.casillas[x][y].contiene.obtenerEstado();
+	}
+
+	this.marcarEstado = function(estado, x, y){
+		this.casillas[x][y].contiene = estado;
+	}
+
+	this.crearTablero(size);
+
+}
+
+function Casilla(x, y){
+	this.x = x;
+	this.y = y;
+
+	this.contiene = new Agua();
+}
+
+function Barco(nombre, tam){
+	this.nombre = nombre;
+	this.tam = tam;
+	this.desplegado = false;
+	this.estado = "intacto";
+	this.disparos = 0;
+	this.orientacion; //horizontal, vertical
+	//Se asume que todas las orientaciones son horizontales
+
+	this.esAgua = function(){
+		return false;
+	}
+
+	this.meDisparan = function(){
+		this.disparos++;
+		if(this.disparos<this.tam){
+			this.estado = "tocado";
+			console.log("Tocado");
+		}
+		else {
+			this.estado = "hundido";
+			console.log("Hundido");
+		}
+	}
+
+	this.obtenerEstado = function(){
+		return this.estado;
+	}
+}
+
+function Agua(){
+	this.nombre = "Agua";
+	
+	this.esAgua = function(){
+		return true;
+	}
+
+	this.meDisparan = function(){
+		console.log("Agua");
+	}
+
+	this.obtenerEstado = function(){
+		return "Agua";
+	}
 }
 
 module.exports.Juego = Juego;
