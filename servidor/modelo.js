a0324936b49311c790a73965b8053fb2014d9c7b
FÉLIX CUEVAS MEDINA <Felix.Cuevas@alu.uclm.es>
2023-07-02 17:47:54 +0200

    Fin cambios

--- servidor/modelo.js
+++ servidor/modelo.js
@@ -178,9 +178,9 @@ function Usuario(nick,juego){
 		this.flota["b2"]=new Barco("b2",2, new Horizontal());
 		this.flota["b4"]=new Barco("b4",4, new Horizontal());
 		// otros barcos: 1, 3, 5,...
-		this.flota["b1"]=new Barco("b1",1, new Horizontal());
-		this.flota["b3"]=new Barco("b3",3, new Horizontal());
-		this.flota["b5"]=new Barco("b5",5, new Horizontal());
+		//this.flota["b1"]=new Barco("b1",1, new Horizontal());
+		//this.flota["b3"]=new Barco("b3",3, new Horizontal());
+		//this.flota["b5"]=new Barco("b5",5, new Horizontal());
 		//this.flota["b5"]=new Barco("b5",5);
 	}
 
